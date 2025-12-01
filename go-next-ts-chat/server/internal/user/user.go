d8130136b3dca81f09d68fa489b1a7b664a2ba48
JadenWe <jaden910825@gmail.com>
2025-12-01 15:33:20 -0500

    feat: implement go realtime chat

---
+++ go-next-ts-chat/server/internal/user/user.go
@@ -0,0 +1,42 @@
+package user
+
+import "context"
+
+type User struct {
+	ID       int64  `json:"id" db:"id"`
+	Username string `json:"username" db:"username"`
+	Email    string `json:"email" db:"email"`
+	Password string `json:"password" db:"password"`
+}
+type createUserRequest struct {
+	Username string `json:"username" db:"username"`
+	Email    string `json:"email" db:"email"`
+	Password string `json:"password" db:"password"`
+}
+
+type createUserResponse struct {
+	ID       string `json:"id" db:"id"`
+	Username string `json:"username" db:"username"`
+	Email    string `json:"email" db:"email"`
+}
+
+type LoginUserRequest struct {
+	Email    string `json:"email" db:"email"`
+	Password string `json:"password" db:"password"`
+}
+
+type LoginUserResponse struct {
+	accessToken string
+	ID          string `json:"id" db:"id"`
+	Username    string `json:"username" db:"username"`
+}
+
+type Repository interface {
+	CreateUser(ctx context.Context, user *User) (*User, error)
+	GetUserByEmail(ctx context.Context, email string) (*User, error)
+}
+
+type Service interface {
+	CreateUser(c context.Context, req *createUserRequest) (*createUserResponse, error)
+	Login(c context.Context, request *LoginUserRequest) (*LoginUserResponse, error)
+}
---
+++ go-next-ts-chat/server/internal/ws/ws_handler.go
@@ -0,0 +1,128 @@
+package ws
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+)
+
+type Handler struct {
+	hub *Hub
+}
+
+func NewHandler(h *Hub) *Handler {
+	return &Handler{
+		hub: h,
+	}
+}
+
+type CreateRoomRequest struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+func (h *Handler) CreateRoom(c *gin.Context) {
+	var request CreateRoomRequest
+
+	if err := c.ShouldBindJSON(&request); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	h.hub.Rooms[request.ID] = &Room{
+		ID:      request.ID,
+		Name:    request.Name,
+		Clients: make(map[string]*Client),
+	}
+
+	c.JSON(http.StatusOK, request)
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		// orgin := r.Header.Get("Origin")
+		// return orgin == "http://localhost:3000"
+		return true
+	},
+}
+
+func (h *Handler) JoinRoom(c *gin.Context) {
+	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	roomID := c.Param("roomID")
+	clientID := c.Query("userId")
+	username := c.Query("username")
+
+	cl := &Client{
+		Conn:     conn,
+		Message:  make(chan *Message, 10),
+		ID:       clientID,
+		RoomID:   roomID,
+		Username: username,
+	}
+
+	m := &Message{
+		Content:  "A new user has joined the room",
+		RoomID:   roomID,
+		Username: username,
+	}
+
+	// Register a new client through the register channel
+	h.hub.Register <- cl
+
+	// Broadcast that message
+	h.hub.Broadcast <- m
+
+	go cl.writeMessage()
+	cl.readMessage(h.hub)
+}
+
+type RoomResponse struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+func (h *Handler) GetRooms(c *gin.Context) {
+	rooms := make([]RoomResponse, 0)
+
+	for _, r := range h.hub.Rooms {
+		rooms = append(rooms, RoomResponse{
+			ID:   r.ID,
+			Name: r.Name,
+		})
+	}
+
+	c.JSON(http.StatusOK, rooms)
+}
+
+type ClientResponse struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
+}
+
+func (h *Handler) GetClients(c *gin.Context) {
+	var clients []ClientResponse
+	roomId := c.Param("roomId")
+
+	if _, ok := h.hub.Rooms[roomId]; !ok {
+		clients = make([]ClientResponse, 0)
+		c.JSON(http.StatusOK, clients)
+	}
+
+	for _, c := range h.hub.Rooms[roomId].Clients {
+		clients = append(clients, ClientResponse{
+			ID:       c.ID,
+			Username: c.Username,
+		})
+	}
+
+	c.JSON(http.StatusOK, clients)
+}
